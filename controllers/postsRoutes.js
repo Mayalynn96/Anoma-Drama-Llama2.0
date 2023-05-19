396b53cacbc1d0dca60654fb9f00ed72c1c791d0
Maya Lynn <mayalynncohen@gmail.com>
2023-05-18 20:45:37 -0700

    finished post routes

--- controllers/postsRoutes.js
+++ controllers/postsRoutes.js
@@ -6,30 +6,22 @@ const { findAll } = require("../models/User");
 // Get All Public Posts with user name and Llama (Anonymous changed to Anoma Llama)
 router.get("/", async (req, res) => {
   try {
-    const allPublicPosts = await Post.findAll({
+    const allPosts = await Post.findAll({
       attributes: ["id", "title", "text", "type", "visibility", "createdAt"],
-      where: { visibility: "public" },
+      where: { visibility: ["public", "anonymous"] },
       include: [
         { model: User, attributes: ["username"], include: [{ model: Llama, attributes: ["llama_image", "llama_hat_image"] }] },
-        { model: Mood, attributes: ["mood"]}
-      ]
-    })
-    const allAnonymousPosts = await Post.findAll({
-      attributes: ["id", "title", "text", "type", "visibility", "createdAt"],
-      where: { visibility: "anonymous" },
-      include: [
-        { model: User, attributes: ["username"], include: [{ model: Llama, attributes: ["llama_image", "llama_hat_image"] }] },
-        { model: Mood, attributes: ["mood"]}
+        { model: Mood, attributes: ["mood"] }
       ]
     })
 
-    allAnonymousPosts.forEach(post => {
-      post.User.username = "Anoma Llama"
-      post.User.Llama.llama_image = "https://raw.githubusercontent.com/Mayalynn96/Anoma-Drama-Llama2.0/Dev/public/images/pixel-llamas/Anoma-llama.png"
-      post.User.Llama.llama_hat_image = "https://raw.githubusercontent.com/Mayalynn96/Anoma-Drama-Llama2.0/Dev/public/images/pixel-llamas/no-hat.png"
+    allPosts.forEach(post => {
+      if (post.visibility === "anonymous") {
+        post.User.username = "Anoma Llama"
+        post.User.Llama.llama_image = "https://raw.githubusercontent.com/Mayalynn96/Anoma-Drama-Llama2.0/Dev/public/images/pixel-llamas/Anoma-llama.png"
+        post.User.Llama.llama_hat_image = "https://raw.githubusercontent.com/Mayalynn96/Anoma-Drama-Llama2.0/Dev/public/images/pixel-llamas/no-hat.png"
+      }
     })
-
-    const allPosts = allPublicPosts.concat(allAnonymousPosts)
 
     res.json(allPosts.sort(function (a, b) {
       return new Date(b.createdAt) - new Date(a.createdAt);
@@ -40,25 +32,31 @@ router.get("/", async (req, res) => {
   };
 });
 
-// router.get("/:id", (req, res) => {
-//   Post.findOne({
-//     where: {
-//       id: req.params.id,
-//     },
-//     include: [Users, Comments],
-//   })
-//     .then((dbPostData) => {
-//       if (!dbPostData) {
-//         res.status(404).json({ message: "No post found with this id" });
-//         return;
-//       }
-//       res.json(dbPostData);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+// Get all curretn users posts
+router.get("/currentUserPosts", async (req, res) => {
+  const token = req.headers?.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ msg: "You must be logged in to add a post!" });
+  }
+  try {
+    const tokenData = jwt.verify(token, process.env.JWT_SECRET);
+
+    const allPosts = await Post.findAll({
+      where: {UserId: tokenData.id},
+      attributes: ["id", "title", "text", "type", "visibility", "createdAt"],
+      include: [
+        { model: Mood, attributes: ["mood"] }
+      ]
+    })
+
+    res.json(allPosts.sort(function (a, b) {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    }))
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error getting all posts!" })
+  };
+});
 
 // Add post
 router.post("/", async (req, res) => {
@@ -106,24 +104,26 @@ router.put("/:id", async (req, res) => {
       return res.status(404).json({ msg: "No post found with this id" });
     }
 
-    if(tokenData.id!=postToEdit.UserId){
-      return res.status(403).json({ msg: "You can only modify your own post"})
+    if (tokenData.id != postToEdit.UserId) {
+      return res.status(403).json({ msg: "You can only modify your own post" })
     }
 
-    const oldMoods = await Mood.findAll({where: {PostId : req.params.id }})
+    const oldMoods = await Mood.findAll({ where: { PostId: req.params.id } })
 
     oldMoods.forEach(mood => {
-      if(!req.body.moods.includes(mood.mood)){
+      if (!req.body.moods.includes(mood.mood)) {
         mood.destroy()
       }
     })
 
-    req.body.moods.forEach(async(mood) => {
-      const existingMood = await Mood.findOne({where: [
-        {mood: mood},
-        {PostId: req.params.id}
-      ]})
-      if(!existingMood){
+    req.body.moods.forEach(async (mood) => {
+      const existingMood = await Mood.findOne({
+        where: [
+          { mood: mood },
+          { PostId: req.params.id }
+        ]
+      })
+      if (!existingMood) {
         const newMood = await Mood.create({
           mood: mood,
           UserId: tokenData.id,
@@ -140,8 +140,8 @@ router.put("/:id", async (req, res) => {
         id: req.params.id
       }
     })
-    
-    return res.json({ msg: "Post and moods have been edited."})
+
+    return res.json({ msg: "Post and moods have been edited." })
 
   } catch (error) {
     console.error(error);
@@ -162,8 +162,8 @@ router.delete("/:id", async (req, res) => {
       return res.status(404).json({ msg: "No post found with this id" });
     }
 
-    if(tokenData.id!=postToDelete.UserId){
-      return res.status(403).json({ msg: "You can only delete your own post"})
+    if (tokenData.id != postToDelete.UserId) {
+      return res.status(403).json({ msg: "You can only delete your own post" })
     } else {
       postToDelete.destroy()
     }
@@ -174,7 +174,7 @@ router.delete("/:id", async (req, res) => {
       }
     })
 
-    return res.json({ msg: "Post and associated moods have been removed."})
+    return res.json({ msg: "Post and associated moods have been removed." })
 
   } catch (error) {
     console.error(error);
